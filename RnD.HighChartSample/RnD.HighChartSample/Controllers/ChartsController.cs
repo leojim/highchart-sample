bbd7fb05a06ecd76705ed71e7ddbfbd4c7f0f4b3
Rasel Ahmmed Bappi <rasel.bappi@hotmail.com>
2013-08-12 09:55:35 +0600

    2013-08-12

--- RnD.HighChartSample/RnD.HighChartSample/Controllers/ChartsController.cs
+++ RnD.HighChartSample/RnD.HighChartSample/Controllers/ChartsController.cs
@@ -17,21 +17,21 @@ namespace RnD.HighChartSample.Controllers
             return View();
         }
 
-        public JsonResult GetPieData()
-        {
-            var chartsViewModels = new List<PieChartViewModel>(){
+        //public JsonResult GetPieData()
+        //{
+        //    var chartsViewModels = new List<PieChartViewModel>(){
 
-               new PieChartViewModel{Name = "Firefox", Data = 45m},
-               new PieChartViewModel{Name = "IE", Data = 26.8m},
-               new PieChartViewModel{Name = "Chrome", Data = 12.8m},
-               new PieChartViewModel{Name = "Safari", Data = 8.5m},
-               new PieChartViewModel{Name = "Opera", Data = 6.2m},
-               new PieChartViewModel{Name = "Others", Data = 0.7m}
+        //       new PieChartViewModel{Name = "Firefox", Data = 45m},
+        //       new PieChartViewModel{Name = "IE", Data = 26.8m},
+        //       new PieChartViewModel{Name = "Chrome", Data = 12.8m},
+        //       new PieChartViewModel{Name = "Safari", Data = 8.5m},
+        //       new PieChartViewModel{Name = "Opera", Data = 6.2m},
+        //       new PieChartViewModel{Name = "Others", Data = 0.7m}
 
-            };
+        //    };
 
-            return Json(chartsViewModels, JsonRequestBehavior.AllowGet);
-        }
+        //    return Json(chartsViewModels, JsonRequestBehavior.AllowGet);
+        //}
 
         //LineCharts
         public ActionResult LineCharts()
@@ -50,14 +50,14 @@ namespace RnD.HighChartSample.Controllers
         {
             string[] _months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
-            List<LineChartSerieViewModel> series = new List<LineChartSerieViewModel>();
+            List<ChartSerieViewModel> series = new List<ChartSerieViewModel>();
 
             for (int i = 0; i < 5; i++)
             {
                 decimal d = 2 + i;
                 decimal[] _datas = { d, 6.90m, 9.50m, 14.50m, 18.20m, 4.56m, d, 26.50m, 23.30m, 18.30m, 13.90m, d };
 
-                LineChartSerieViewModel lineChartSerieViewModel = new LineChartSerieViewModel();
+                ChartSerieViewModel lineChartSerieViewModel = new ChartSerieViewModel();
                 lineChartSerieViewModel.SeriesName = "Tokyo : " + i;
                 lineChartSerieViewModel.SeriesDatas = _datas;
 
@@ -99,5 +99,77 @@ namespace RnD.HighChartSample.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult ServerBarChartsData()
+        {
+            string[] _categories = { "Apples", "Oranges", "Pears", "Grapes", "Bananas" };
+
+            List<ChartSerieViewModel> series = new List<ChartSerieViewModel>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                decimal d = 2 + i;
+                decimal[] _datas = { d, 3, 4, 7, d };
+
+                ChartSerieViewModel lineChartSerieViewModel = new ChartSerieViewModel();
+                lineChartSerieViewModel.SeriesName = "John : " + i;
+                lineChartSerieViewModel.SeriesDatas = _datas;
+
+                series.Add(lineChartSerieViewModel);
+            }
+
+            var model = new BarChartViewModel();
+
+            model.TitleText = "Stacked column chart";
+            model.XAxisCategories = _categories;
+            model.YAxisTitleText = "Total fruit consumption";
+            model.Series = series.ToArray();
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        //PieCharts
+        public ActionResult PieCharts()
+        {
+            return View();
+        }
+
+        //ServerPieCharts
+        public ActionResult ServerPieCharts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ServerPieChartsData()
+        {
+            List<PieChartSerieDataViewModel> seriesDatas = new List<PieChartSerieDataViewModel>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                decimal d = 5 * i;
+
+                PieChartSerieDataViewModel pieChartSerieDataViewModel = new PieChartSerieDataViewModel();
+                pieChartSerieDataViewModel.DataName = "Tokyo : " + i;
+                pieChartSerieDataViewModel.DataY = d;
+
+                seriesDatas.Add(pieChartSerieDataViewModel);
+            }
+
+            var model = new PieChartViewModel();
+
+            model.TitleText = "Browser market shares at a specific website, 2010";
+            model.SeriesDatas = seriesDatas.ToArray();
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        //ReloadCharts
+        public ActionResult ReloadCharts()
+        {
+            return View();
+        }
+
     }
 }
--- RnD.HighChartSample/RnD.HighChartSample/ViewModels/ChartsViewModel.cs
+++ RnD.HighChartSample/RnD.HighChartSample/ViewModels/ChartsViewModel.cs
@@ -12,18 +12,40 @@ namespace RnD.HighChartSample.ViewModels
         public string[] XAxisCategories { get; set; }
         public string YAxisTitleText { get; set; }
         public string TooltipValueSuffix { get; set; }
-        public LineChartSerieViewModel[] Series { get; set; }
+        public ChartSerieViewModel[] Series { get; set; }
     }
 
-    public class LineChartSerieViewModel
+    public class ChartSerieViewModel
     {
         public string SeriesName { get; set; }
         public decimal[] SeriesDatas { get; set; }
     }
 
+    public class BarChartViewModel
+    {
+        public string TitleText { get; set; }
+        public string[] XAxisCategories { get; set; }
+        public string YAxisTitleText { get; set; }
+        public ChartSerieViewModel[] Series { get; set; }
+    }
+
     public class PieChartViewModel
     {
-        public string Name { get; set; }
-        public decimal Data { get; set; }
+        public string TitleText { get; set; }
+        //public PieChartSerieViewModel[] Series { get; set; }
+        public PieChartSerieDataViewModel[] SeriesDatas { get; set; }
     }
+
+    public class PieChartSerieViewModel
+    {
+        public string SeriesName { get; set; }
+        public PieChartSerieDataViewModel[] SeriesDatas { get; set; }
+    }
+
+    public class PieChartSerieDataViewModel
+    {
+        public string DataName { get; set; }
+        public decimal DataY { get; set; }
+    }
+
 }
